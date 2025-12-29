75ca780620dc33f23c1cc98c6158bd4245bbc5c9
AkshadKavinMars <akshadkavinmars@gmail.com>
2025-12-29 13:38:22 -0500

    Develop Product Feedback survey form using Events

---
+++ productSurvey/product_survey.js
@@ -0,0 +1,31 @@
+const submitButton = document.getElementById('submitBtn');
+submitButton.addEventListener('click', submitFeedback);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    submitFeedback();
+  }
+});
+
+function submitFeedback() {
+  const username = document.getElementById('name').value;
+  const age = document.getElementById('age').value;
+  const email = document.getElementById('email').value;
+  const job = document.getElementById('job').value;
+  const designation = document.getElementById('designation').value;
+  const productType = document.getElementById('productType').value;
+  const feedback = document.getElementById('feedbackText').value;
+  const experience = document.getElementById('expText').value;
+  alert('Thank you');
+
+  document.getElementById('userName').innerHTML = username;
+  document.getElementById('userAge').innerHTML = age;
+  document.getElementById('userEmail').innerHTML = email;
+  document.getElementById('userJob').innerHTML = job;
+  document.getElementById('userDesignation').innerHTML = designation;
+  document.getElementById('userProductChoice').innerHTML = productType;
+  document.getElementById('userFeedback').innerHTML = feedback;
+  document.getElementById('expUser').innerHTML = experience;
+  document.getElementById('userInfo').style.display = 'block';
+  
+}
