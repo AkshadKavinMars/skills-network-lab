781be91c73f15fbd9fe636a605a5212126f30907
AkshadKavinMars <akshadkavinmars@gmail.com>
2026-01-11 21:00:18 +0530

    Classes and Objects (Sample Code)

--- ClassesObjectsSampleCode/COSampleCode.js
+++ ClassesObjectsSampleCode/COSampleCode.js
@@ -13,5 +13,21 @@ class person {
 const p1 = new person()
 p1.firstName = "Akshad"
 p1.lastName = "Kavin"
-console.log(p1.firstName); console.log(p1.lastName)
+p1.getResult()
+class Car {
+    constructor(model,year,mode) {
+        this.model = model
+        this.year = year
+        this.mode = mode
+    }
 
+    getCar() {
+        console.log(`Car Model - ${this.model}, Year - ${this.year}, mode - ${this.mode}`)
+    }
+}
+
+const c = new Car()
+c.model = "Maruti DZire"
+c.year = "2026"
+c.mode = "Manual"
+c.getCar()
