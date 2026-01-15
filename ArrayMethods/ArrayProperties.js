ac05867532ed973125dd035adb61c6144e5fe9fd
AkshadKavinMars <akshadkavinmars@gmail.com>
2026-01-15 19:03:03 +0530

    Accessing Array using Properties and Methods

--- ArrayMethods/ArrayProperties.js
+++ ArrayMethods/ArrayProperties.js
@@ -1,4 +1,4 @@
-//Using forEach function [ Printing Names]
+// Using forEach function [ Printing Names]
 
 const name = [
     {"name" : "Akshad" , "class" : "M.Tech"},
@@ -6,13 +6,13 @@ const name = [
     ]
 name.forEach(names => console.log(names.name))
 
-//Using For Loop [Printing Class]
+// Using For Loop [Printing Class]
 
 for(let i = 0 ; i<name.length ; i++) {
     console.log(name[i].class)
 }
 
-//Creating an Array and Iterating it
+// Creating an Array and Iterating it
 
 class StuName {
     constructor(name,classs) {
@@ -31,3 +31,32 @@ class StuName {
     n2.classs = "B.Tech"
     n1.getinfo() 
     n2.getinfo()
+    
+// Store in array
+const students = [n1, n2];
+
+// Access
+students.forEach(student => student.getinfo());
+
+// Using Push Operation
+const Car = [{"Name" : "Yamaha" , "Model" : "DZire" , "Priority" : 3} , {"Name" : "Porsche" , "Model" : "S-Series" , "Priority" : 1 } , {"Name" : "Kia" , "Class" : "B" , "Priority" : 4}]
+
+console.log("Before Push Operation - [Length of the array] - ")
+console.log(Car.length)
+console.log("After Push Operation - Length of the Array -")
+Car.push({"Name": "BMW" , "Class" :"A", "Priority" : 2})
+console.log(Car.length)
+
+// Pop Operation
+console.log(Car.pop())
+
+// Updating Array once again using Pop Method
+Car.push({"Name": "BMW" , "Class" :"A", "Priority" : 2})
+
+// Filtering [Best Priority Cars]
+const filteredCar = Car.filter(cars => cars.Priority < 3);
+console.log(filteredCar)
+
+// Mapping only Car Names
+const CarNames = Car.map(cars => cars.Name)
+console.log(CarNames)
